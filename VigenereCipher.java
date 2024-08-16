be6e2f65b82d2fdd150db117cb568ddd1995e8d6
Yash Malik <148884566+yashmalikk@users.noreply.github.com>
2024-08-17 05:04:18 +0530

    Update VigenereCipher.java

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -6,75 +6,75 @@
 
  class Encoder {
   public static void encodingAlgo(String Pt, String keyword) {
-      for (int i = 0; i < Pt.length(); i++) {
-          char letter = Pt.charAt(i);
-          char keyletter = keyword.charAt(i % keyword.length());
-          int key = (int) keyletter - 'a'; 
-          int asciival = (int) letter + key;
-          if (asciival > 'z') {
-              asciival -= 26;
-          }
-          else if (asciival < 'a') {
-              asciival += 26;
-          }
-          
-          char modletter = (char) asciival;
-          System.out.print(modletter);
+    for (int i = 0; i < Pt.length(); i++) {
+      char letter = Pt.charAt(i);
+      char keyletter = keyword.charAt(i % keyword.length());
+      int key = (int) keyletter - 'a'; 
+      int asciival = (int) letter + key;
+      if (asciival > 'z') {
+          asciival -= 26;
       }
-      System.out.println();
+      else if (asciival < 'a') {
+          asciival += 26;
+      }
+      
+      char modletter = (char) asciival;
+      System.out.print(modletter);
+    }
+    System.out.println();
   }
 }
 
 class Decoder {
   public static void decodingAlgo(String Pt, String keyword) {
-      for (int i = 0; i < Pt.length(); i++) {
-          char letter = Pt.charAt(i);
-          char keyletter = keyword.charAt(i % keyword.length());
-          int key = (int) keyletter - 'a';
-          int asciival = (int) letter - key;
-          if (asciival > 'z') {
-              asciival -= 26;
-          }
-          else if (asciival < 'a') {
-              asciival += 26;
-          }
-          
-          char modletter = (char) asciival;
-          System.out.print(modletter);
+    for (int i = 0; i < Pt.length(); i++) {
+      char letter = Pt.charAt(i);
+      char keyletter = keyword.charAt(i % keyword.length());
+      int key = (int) keyletter - 'a';
+      int asciival = (int) letter - key;
+      if (asciival > 'z') {
+        asciival -= 26;
+        }
+      else if (asciival < 'a') {
+        asciival += 26;
       }
-      System.out.println();
+      
+      char modletter = (char) asciival;
+      System.out.print(modletter);
+  }
+  System.out.println();
   }
 }
 
 class InputHandling {
   public static void argno(String args[]) {
-      if (args.length <= 1 || args.length >3 ) {
-          System.err.println("Incorrect number of parameters");
-          System.out.println();
-          System.exit(2);
-      }
-      if (args.length == 2) {
-        System.out.println(args[1]);
-        System.out.println();
-        System.exit(0);
-      }
-    }
+    if (args.length <= 1 || args.length >3 ) {
+      System.err.println("Incorrect number of parameters");
+      System.out.println();
+      System.exit(2);
+  }
+  if (args.length == 2) {
+    System.out.println(args[1]);
+    System.out.println();
+    System.exit(0);
+  }
+}
 
 
   public static void command(String cmd, String plaintext, String keyword) {
-      if (!cmd.equals("encode") && !cmd.equals("decode")) {
-          System.err.println("Valid options are \"encode\" or \"decode\"");
-          System.out.println();
-          System.exit(1);
-      }
+    if (!cmd.equals("encode") && !cmd.equals("decode")) {
+      System.err.println("Valid options are \"encode\" or \"decode\"");
+      System.out.println();
+      System.exit(1);
+    }
 
-      if (cmd.equals("encode")) {
-        Encoder.encodingAlgo(plaintext, keyword); 
-      }
+    if (cmd.equals("encode")) {
+      Encoder.encodingAlgo(plaintext, keyword); 
+    }
 
-      if (cmd.equals("decode")) {
-        Decoder.decodingAlgo(plaintext, keyword);
-      }
+    if (cmd.equals("decode")) {
+      Decoder.decodingAlgo(plaintext, keyword);
+    }
   }
 }
 
