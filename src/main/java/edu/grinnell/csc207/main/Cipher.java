a6cb5e4aa6909413a4c8b1a7ee09036337acbe24
Yash Malik Lenovo Portable <yash.malik0606@gmail.com>
2024-09-06 09:14:14 -0500

    added a way to store keystring and input sting

--- src/main/java/edu/grinnell/csc207/main/Cipher.java
+++ src/main/java/edu/grinnell/csc207/main/Cipher.java
@@ -5,6 +5,8 @@ import java.io.PrintWriter;
 public class Cipher {
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
+    String inputString = "";
+    String keyString = "";
 
     for (int i = 0; i < args.length; i++) {
       pen.printf("args[%d] = \"%s\"\n", i, args[i]);
@@ -25,6 +27,14 @@ public class Cipher {
 
       if (args[i].startsWith("-d")) {
         // run decode
+      }
+
+      if (inputString == "") {
+        inputString = args[i];
+      }
+
+      if (keyString == "") {
+        keyString = args[i];
       }
     }
     pen.close();
