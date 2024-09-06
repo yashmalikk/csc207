0be3501202958f1d7cc697d676fef6ad5575795b
Yash Malik Lenovo Portable <yash.malik0606@gmail.com>
2024-09-05 22:41:08 -0500

    Added method functions

--- src/main/java/edu/grinnell/csc207/util/CipherUtils.java
+++ src/main/java/edu/grinnell/csc207/util/CipherUtils.java
@@ -2,26 +2,102 @@ package edu.grinnell.csc207.util;
 
 public class CipherUtils {
   private static int letter2int(char letter) {
-    return 0; // STUB
+    int asciival = (int)letter;
+    return asciival; // STUB
   }
 
-  private static int int2letter(int i) {
-    return 'a'; // STUB
+  private static char int2letter(int asciival) {
+    char modletter = (char)asciival;
+    return modletter; // STUB
   }
 
-  public static String caesarEncrypt(String str, char letter) {
-    return str; // STUB
+  public static String caesarEncrypt(String plainText, char key) {
+    String encryptText = new String();
+    for (int i = 0; i < plainText.length(); i++) {
+
+      int asciival = letter2int(plainText.charAt(i)) + letter2int(key);
+
+      if (asciival > 122) {
+        asciival = asciival - 26;
+      }
+
+      else if (asciival < 97) {
+      
+        asciival = asciival + 26;
+      }
+
+      char modletter = int2letter(asciival);
+      encryptText += modletter; 
+    }
+    return encryptText;
   }
 
-  public static String caesarDecrypt(String str, char letter) {
-    return str; // STUB
+  public static String caesarDecrypt(String cipherText, char key) {
+    String decryptText = new String();
+    for (int i = 0; i < cipherText.length(); i++) {
+
+      int asciival = letter2int(cipherText.charAt(i)) - letter2int(key);
+
+      if (asciival > 122) {
+        asciival = asciival - 26;
+      }
+
+      else if (asciival < 97) {
+        asciival = asciival + 26;
+      }
+
+      char modletter = int2letter(asciival);
+      decryptText += modletter; 
+    }
+    return decryptText; // STUB
+  }
+  
+
+  public static String vigenereEncrypt(String plainText, String keyword) {
+    String encryptText = new String();
+
+    for (int i = 0; i < plainText.length(); i++) {
+      char letter = plainText.charAt(i);
+      char keyletter = plainText.charAt(i % keyword.length());
+
+      int key = letter2int(keyletter) - 'a'; 
+      int asciival = letter2int(letter) + key;
+      
+      if (asciival > 'z') {
+        asciival -= 26;
+      }
+
+      else if (asciival < 'a') {
+        asciival += 26;
+      }
+      
+      char modletter = int2letter(asciival);
+      encryptText += modletter;
+    }
+    return encryptText; // STUB
   }
 
-  public static String vigenereEncrypt(String str, String key) {
-    return str; // STUB
-  }
+public static String vigenereDecrypt(String cipherText, String keyword) {
+  String decryptText = new String();
 
-  public static String vigenereDecrypt(String str, String key) {
-    return str;
+    for (int i = 0; i < cipherText.length(); i++) {
+      char letter = cipherText.charAt(i);
+      char keyletter = keyword.charAt(i % keyword.length());
+
+      int key = letter2int(keyletter) - 'a';
+      int asciival = letter2int(letter) - key;
+
+      if (asciival > 'z') {
+        asciival -= 26;
+      }
+
+      else if (asciival < 'a') {
+        asciival += 26;
+      }
+      
+      char modletter = (char) asciival;
+      decryptText += modletter;
+    }
+    return decryptText;
   }
 }
